cfe22b1310cbb28838024db74fc32b73e0680f2a
Sam Clavette <samclavette@yahoo.com>
2020-08-10 11:37:52 -0500

    Initial commit. I created a list of words and then made a function to print the words that contained th.

---
+++ LINQProblems/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+            var wordsContainingTH = words.Where(w => w.Contains("th"));
+            foreach (var word in wordsContainingTH)
+            {
+                Console.WriteLine(word);
+                Console.ReadLine();
+            }
+        }
+    }
+}
