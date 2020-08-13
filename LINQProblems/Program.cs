5996686d38f5c599b71f71a5272885488408fce4
Sam Clavette <samclavette@yahoo.com>
2020-08-13 00:12:19 -0500

    attempted problems 3 and 4 without using LINQ

--- LINQProblems/Program.cs
+++ LINQProblems/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,42 +12,50 @@ namespace LINQProblems
     {
         static void Main(string[] args)
         {
-            //var words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
-            //var wordsContainingTH = words.Where(w => w.Contains("th"));
-            ////foreach (var word in wordsContainingTH)
-            ////{
-            ////    Console.WriteLine(word);
-            ////    Console.ReadLine();
-            ////}
+            var words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+            var wordsContainingTH = words.Where(w => w.Contains("th"));
+            foreach (var word in wordsContainingTH)
+            {
+                Console.WriteLine(word);
+                Console.ReadLine();
+            }
 
-            //var names = new List<string>() { "Mike", "Brad", "Nevin", "Zack", "Mike" };
-            //var copiedList = names.Distinct();
-            //foreach (var name in copiedList)
-            //{
-            //    Console.WriteLine(name);
-            //    Console.ReadLine();
-            //}
-
-            //List<string> classGrades = new List<string>()
-            //{"80,100,92,89,65",
-            // "93,81,78,84,69",
-            // "73,88,83,99,64",
-            // "98,100,66,74,55"};
-            //var newGrades = classGrades.Where(g => g.;
+            var names = new List<string>() { "Mike", "Brad", "Nevin", "Zack", "Mike" };
+            var copiedList = names.Distinct();
+            foreach (var name in copiedList)
+            {
+                Console.WriteLine(name);
+                Console.ReadLine();
+            }
 
             // for each string
             // make string into an array
-            // remove the min of the array
+            // convert elements to doubles
+            // order by lowest to highest
+            // remove the first of the array
+            // average the remaining array
+            // average all 4 array averages
 
-            string grades = "80,100,92,89,65";
-            string[] split = grades.Split(new Char[] { ',' });
-            
-            foreach (var grade in split)
+            List<string> classGrades = new List<string>()
+            {"80,100,92,89,65",
+             "93,81,78,84,69",
+             "73,88,83,99,64",
+             "98,100,66,74,55"};
+            foreach (var set in classGrades)
             {
-                Convert.ToInt32(grade);
+                var gradeList = set.Split(',');
+                foreach (var grade in gradeList)
+                {
+                    Convert.ToDouble(grade);
+                }
             }
-            int lowerGrade = split.Min();
 
+            var mike = "TERRILL";
+            var array = mike.ToCharArray();
+            foreach (var c in array)
+            {
+                
+            }
         }
     }
 }
